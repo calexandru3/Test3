2d19e2967e33ee8f72d91a09e77f9ae4a82f81b9
Florin Precup <florin.precup@itsmartsystems.eu>
2019-12-05 16:57:26 +0200

    empty data source view

---
+++ Test3/Test3/CellWithField.swift
@@ -0,0 +1,19 @@
+//
+//  CellWithField.swift
+//  Test3
+//
+//  Created by Bogdan Marica on 05/12/2019.
+//  Copyright © 2019 Florin Precup. All rights reserved.
+//
+
+import UIKit
+
+class CellWithField: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Test3/Test3/ViewController.swift
+++ Test3/Test3/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import EmptyDataSet_Swift
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, EmptyDataSetSource, EmptyDataSetDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    let dataSource:Array<String> = {
+        let arry = Array<String>()
+
+        return arry
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.emptyDataSetSource = self
+        tableView.emptyDataSetDelegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.emptyDataSetView { view in
+            view.titleLabelString(NSAttributedString(string: "Niciun oras inserat in lista de preferinte!"))
+        }
+    }
+
+    @IBAction func addCity(_ sender: Any) {
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
+
+extension ViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 
 
 }
-
